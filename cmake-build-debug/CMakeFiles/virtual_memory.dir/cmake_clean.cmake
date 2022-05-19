7eee83ee093b045785d75dbde731ca7857972bc7
Oren Gercenshtei <oren_gerc@cs.huji.ac.il>
2022-05-19 07:56:31 +0300

    add main

---
+++ cmake-build-debug/CMakeFiles/virtual_memory.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/virtual_memory.dir/main.cpp.o"
+  "virtual_memory"
+  "virtual_memory.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/virtual_memory.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
